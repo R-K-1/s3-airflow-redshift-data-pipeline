042a839e2e03d3355a925ed41e0f60b9871d6547
root <raymondkalonji@gmail.com>
2020-09-25 01:02:05 +0000

    feat: scheduling dag hourly run

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -20,7 +20,7 @@ default_args = {
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval=None,
+          schedule_interval="0 * * * *",
           max_active_runs=1
         )
 
